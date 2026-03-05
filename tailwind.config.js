d26af05159d31d67fcb18c32c4804f2a96856b60
Azid-ops <mahad5063@gmail.com>
2026-03-05 10:17:12 +0500

    Added tools

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  theme: { extend: {
+    keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-6px)' },
+        },
+      },
+      animation: {
+        float: 'float 2s ease-in-out infinite',
+      },
+  } },
   plugins: [],
 }
